9c4cc4a545870c534205d6603c9bf784feddb47e
MarinaChorna <mrnchrn06@gmail.com>
2023-02-18 18:55:40 +0200

    Завдання 2

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,16 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
-console.log(galleryItems);
+const galleryEl = document.querySelector('ul.gallery');
+
+const galleryItemsEl = galleryItems.map(({preview,original,description})=>{
+    return `<a class="gallery__item" href="${original}">
+    <img class="gallery__image" src="${preview}" alt="${description}" />
+  </a>`
+}).join('');
+galleryEl.insertAdjacentHTML('beforeend', galleryItemsEl);
+
+const gallery = new SimpleLightbox('.gallery a', {
+    captionsData: 'alt',
+    captionDelay: 250,
+});
